3691ae41b3746aec2f218ef81d6951338cd9f534
shihaijie <shjborage@gmail.com>
2014-09-02 13:53:53 +0800

    Add iOS  project

---
+++ iOS/iBeaconScanner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iBeaconScanner
+//
+//  Created by shihaijie on 9/2/14.
+//  Copyright (c) 2014 Saick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/iBeaconScanner/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iBeaconScanner
+//
+//  Created by shihaijie on 9/2/14.
+//  Copyright (c) 2014 Saick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+@end
